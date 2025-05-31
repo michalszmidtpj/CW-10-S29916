fbce04411b28dd919b369cb6b4bc927b145b222f
michalszmidtpj <s29916@pjwstk.edu.pl>
2025-05-31 19:05:14 +0200

    Initial commit after scaffold

---
+++ CW-10-S29916/Controllers/TripsController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW_10_S29916.Controllers;
+
+public class TripsController : ControllerBase
+{
+    
+}
---
+++ CW-10-S29916/Services/DbService.cs
@@ -0,0 +1,10 @@
+namespace CW_10_S29916.Services;
+
+public interface IDbService
+{
+}
+
+public class DbService : IDbService
+{
+    
+}
